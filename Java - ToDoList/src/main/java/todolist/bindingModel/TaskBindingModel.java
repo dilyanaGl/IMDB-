3202e68531bd8d742002e787bd605bd4f205640e
Dilyana94 <dilyana_stz@abv.bg>
2017-12-20 07:58:29 +0200

    Java - To Do List

---
+++ Java - ToDoList/src/main/java/todolist/bindingModel/TaskBindingModel.java
@@ -0,0 +1,25 @@
+package todolist.bindingModel;
+
+public class TaskBindingModel {
+    private String Title;
+
+    private String Comments;
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getComments() {
+        return Comments;
+    }
+
+    public void setComments(String comments) {
+        Comments = comments;
+    }
+}
+
+
