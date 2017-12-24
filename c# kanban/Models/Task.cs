2db53dbe5535da722d3503bf26aa0e74b93b7db0
Dilyana94 <dilyana_stz@abv.bg>
2017-12-24 18:44:34 +0200

    c#

---
+++ c# kanban/Models/Task.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.UI.WebControls;
+
+namespace TeisterMask.Models
+{
+    public class Task
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Status { get; set; }
+
+    }
+}
